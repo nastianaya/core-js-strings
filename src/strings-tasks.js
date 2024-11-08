8117a74f8e94d17ff4ec32705d97e3e09a4dadab
Анастасия Чернышева <naya@MacBook-Air-Anastasia.local>
2024-11-08 23:57:18 +0300

    fix: fourth solution

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -369,8 +369,8 @@ function findLongestWord(/* sentence */) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  return str.split(' ').reverse().join(' ').split('').reverse().join('');
 }
 
 /**
